6ae1549d7962a4615392b9d5f4087642fecaadfe
Augustas Budnikas <budnikiukas@gmail.com>
2024-04-13 16:19:51 +0300

    Add Dockerfile

--- frontend/SendIT/src/components/InputField/InputField.tsx
+++ frontend/SendIT/src/components/InputField/InputField.tsx
@@ -10,7 +10,7 @@ export const InputField = ({ type, id, label } : InputFieldProps) => {
   return (
     <div className={styles.formField}>
         <input className={styles.formInput} type={type} id={id} placeholder=''/>
-        <label> {label} </label>
+        <label>{ label }</label>
     </div>
   );
 };
