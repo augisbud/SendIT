516a4605e7bf13db3da8f43ecda666bb1133ce41
Edvinas Burba <edvinas.burba@mif.stud.vu.lt>
2024-04-20 18:09:45 +0300

    Global scss file, headings file and const variables were added to the project

--- frontend/SendIT/src/App.tsx
+++ frontend/SendIT/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from "./pages/Home";
-import { Login } from "./pages/Login/Login"
+import { Login } from "./pages/Login/Login";
+
+import "./globals.scss";
 
 export const App = () => (
   <BrowserRouter>
