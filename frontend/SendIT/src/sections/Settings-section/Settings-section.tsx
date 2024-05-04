483d75d747b059cdf8b46f89d8dbeb0124ef38be
Edvinas Burba <edvinas.burba@mif.stud.vu.lt>
2024-05-04 22:00:30 +0300

    Added change password form

--- frontend/SendIT/src/sections/Settings-section/Settings-section.tsx
+++ frontend/SendIT/src/sections/Settings-section/Settings-section.tsx
@@ -16,7 +16,7 @@ export const SettingsSection = () => {
                     </div>
                     <div className={styles.usernameFormField}>
                         <label>Enter your password to confirm changes.</label>
-                        <input type="text" name="username" placeholder="Confirm changes with password"></input>
+                        <input type="password" name="confirm-password" placeholder="Confirm changes with password"></input>
                     </div>
 
                     <Button style={{ padding: "1rem 4.5rem", fontSize: "16px" }}>Apply changes</Button>
@@ -25,15 +25,15 @@ export const SettingsSection = () => {
 
              {/* Password section */}
              <div className={styles.usernameFormContainer}>
-                <h3>Update your username</h3>
+                <h3>Change password</h3>
                 <form className={styles.usernameForm}>
                     <div className={styles.usernameFormField}>
-                        <label>Enter your desired usename.</label>
-                        <input type="text" name="username" placeholder="New username"></input>
+                        <label>Enter your current password.</label>
+                        <input type="password" name="password" placeholder="Current password"></input>
                     </div>
                     <div className={styles.usernameFormField}>
-                        <label>Enter your password to confirm changes.</label>
-                        <input type="text" name="username" placeholder="Confirm changes with password"></input>
+                        <label>Enter your new password.</label>
+                        <input type="password" name="new-password" placeholder="New password"></input>
                     </div>
 
                     <Button style={{ padding: "1rem 4.5rem", fontSize: "16px" }}>Apply changes</Button>
