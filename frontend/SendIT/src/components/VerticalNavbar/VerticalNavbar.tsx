0495281b3b722d99c22bc514cb0d163ce9bae505
Leonardas <leonardas.sinkevicius@mif.stud.vu.lt>
2024-05-06 14:31:39 +0300

    Modified vertical navbar

--- frontend/SendIT/src/components/VerticalNavbar/VerticalNavbar.tsx
+++ frontend/SendIT/src/components/VerticalNavbar/VerticalNavbar.tsx
@@ -4,16 +4,18 @@ import styles from "./VerticalNavbar.module.scss";
 import { Link } from "react-router-dom";
 import { Logo } from "../Logo/Logo";
 
-import chat from "../../assets/chat.svg";
-import friend from "../../assets/friend.svg";
-import mode from "../../assets/mode.svg";
-import settings from "../../assets/settings.svg";
+import chatWhite from "../../assets/chat-white.svg";
+import chatBlue from "../../assets/chat-blue.svg";
+import friendWhite from "../../assets/friend-white.svg";
+import friendBlue from "../../assets/friend-blue.svg";
+import modeWhite from "../../assets/mode-white.svg";
+import modeBlue from "../../assets/mode-blue.svg";
+import settingsWhite from "../../assets/settings-white.svg";
+import settingsBlue from "../../assets/settings-blue.svg";
 import logout from "../../assets/logout.svg";
 
 export const VerticalNavbar = () => {
-  const location = useLocation();
-
-  const path = location.pathname;
+  const { pathname } = useLocation();
 
   return (
     <div className={styles.navbar}>
@@ -22,32 +24,32 @@ export const VerticalNavbar = () => {
 
         <ul className={styles.navbarMenu}>
           <li className={styles.navbarItem}>
-            <Link className={styles.navbarLink} to={"/chat"}>
-              <img src={chat} alt="chat" className={`${path === "/chat" ? styles.active : ""}`}/>
+            <Link className={styles.navbarLink} to="/chat">
+            <img src={pathname === "/chat" ? chatBlue : chatWhite} alt="chat" />
             </Link>
           </li>
           <li className={styles.navbarItem}>
-            <Link className={styles.navbarLink} to={"/add-friend"}>
-                <img src={friend} alt="friend" />
+            <Link className={styles.navbarLink} to="/add-friend">
+              <img src={pathname === "/add-friend" ? friendBlue : friendWhite} alt="friend" />
             </Link>
           </li>
           <li className={styles.navbarItem}>
-            <Link className={styles.navbarLink} to={"/"}>
-                <img src={mode} alt="mode" />
+            <Link className={styles.navbarLink} to="/mode">
+              <img src={pathname === "/mode" ? modeBlue : modeWhite} alt="mode" />
             </Link>
           </li>
           <li className={styles.navbarItem}>
-            <Link className={styles.navbarLink} to={"/settings"}>
-                <img src={settings} alt="settings" />
+            <Link className={styles.navbarLink} to="/settings">
+              <img src={pathname === "/settings" ? settingsBlue : settingsWhite} alt="settings" />
             </Link>
           </li>
         </ul>
       </div>
       <li className={styles.Logout}>
-            <Link className={styles.navbarLink} to={"/"}>
-                <img src={logout} alt="logout" />
-            </Link>
-          </li>
+        <Link className={styles.navbarLink} to="/logout">
+            <img src={logout} alt="logout" />
+        </Link>
+      </li>
     </div>
   );
 };
