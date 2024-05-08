f45896003a9c87e58c0be289ea30364f8b15e1b0
Edvinas Burba <edvinas.burba@mif.stud.vu.lt>
2024-05-08 20:28:33 +0300

    .

--- frontend/SendIT/src/pages/Chat/Chat.tsx
+++ frontend/SendIT/src/pages/Chat/Chat.tsx
@@ -10,7 +10,7 @@ export const Chat = () => {
 
             <div className={styles.chatWrapper}>
                 <Inbox />
-                <Conversation />
+                <Conversation/>
             </div>
         </main>
     );
