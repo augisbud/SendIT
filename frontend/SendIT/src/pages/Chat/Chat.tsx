a26a6639a294c0dce2c44b52853d5bbd983c4f66
Edvinas Burba <edvinas.burba@mif.stud.vu.lt>
2024-05-08 18:14:35 +0300

    .

--- frontend/SendIT/src/pages/Chat/Chat.tsx
+++ frontend/SendIT/src/pages/Chat/Chat.tsx
@@ -2,19 +2,8 @@ import styles from "./Chat.module.scss";
 import { VerticalNavbar } from "../../components/VerticalNavbar/VerticalNavbar";
 import { Inbox } from "../../sections/Inbox/Inbox";
 import { Conversation } from "../../sections/Conversation/Conversation";
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from "react";
 
 export const Chat = () => {
-    // const navigate = useNavigate();
-
-    // // Check if the user is authenticated
-    // const authToken = localStorage.getItem("authToken");
-    // if (!authToken) {
-    //     navigate("/signup");
-    // }
-
-
     return (
         <main>
             <VerticalNavbar />
