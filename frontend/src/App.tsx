263246b6d28a3517b2845bf98a59a956dbbd2173
Augustas Budnikas <budnikiukas@gmail.com>
2024-05-21 21:27:17 +0300

    PROD Ready

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { Home } from "./pages/Home";
 import { Login } from "./pages/Login/Login";
 import { SignUp } from './pages/SignUp/SignUp';
 import { Chat } from './pages/Chat/Chat';
-import { Settings } from './pages/Settings/Settings';
 
 import "./globals.scss";
 import { useEffect } from 'react';
--- frontend/src/pages/UnderConstruction/UnderConstruction.tsx
+++ frontend/src/pages/UnderConstruction/UnderConstruction.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import styles from './UnderConstruction.module.scss'
 import underCon from '../../assets/underCon.png'
-
 
 export const UnderConstruction = () => {
     return (
