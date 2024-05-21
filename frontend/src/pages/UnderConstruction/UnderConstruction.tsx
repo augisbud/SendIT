2c3d6721360c482aafc30b39d383b160b001a38b
Leonardas <leonardas.sinkevicius@mif.stud.vu.lt>
2024-05-21 21:59:19 +0300

    Construction site updated

--- frontend/src/pages/UnderConstruction/UnderConstruction.tsx
+++ frontend/src/pages/UnderConstruction/UnderConstruction.tsx
@@ -1,9 +1,11 @@
 import styles from './UnderConstruction.module.scss'
 import underCon from '../../assets/underCon.png'
+import { VerticalNavbar } from '../../components/VerticalNavbar/VerticalNavbar'
 
 export const UnderConstruction = () => {
     return (
         <div className={styles.container}>
+            <VerticalNavbar />
             <div className={styles.content}>
                 <h1>We're Building Something Great!</h1>
                 <p>Our website is currently under construction. Stay tuned for something amazing!</p>
